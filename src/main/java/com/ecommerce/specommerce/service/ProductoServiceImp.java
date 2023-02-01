22e67cc811d24249a7f3628eb0169c165ebdde86
toolsboxhead <jesus_im.salvador@protonmail.com>
2023-01-31 20:50:13 -0400

    Creacion de plantilla para CRUD de Producto y repository y controller de producto

---
+++ src/main/java/com/ecommerce/specommerce/service/ProductoServiceImp.java
@@ -0,0 +1,43 @@
+package com.ecommerce.specommerce.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.specommerce.model.Producto;
+import com.ecommerce.specommerce.repository.ProductoRepository;
+
+@Service
+public class ProductoServiceImp implements ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+    
+    @Override
+    public Producto save(Producto producto) {
+        
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    public Optional<Producto> get(Integer id) {
+        
+        return productoRepository.findById(id);
+    }
+
+    @Override
+    public void update(Producto producto) {
+        productoRepository.save(producto);
+        
+    }
+
+    @Override
+    public void delete(Integer id) {
+       productoRepository.deleteById(id);
+        
+    }
+
+    
+    
+}
